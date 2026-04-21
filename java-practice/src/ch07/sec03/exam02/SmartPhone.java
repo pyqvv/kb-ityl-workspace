66b50cabd5f59f1da548bd9ddc425ed7b27a5624
seoyeong <pyqw_@naver.com>
2026-04-21 18:48:28 +0900

    Feat: ch07 상속 기본 문제 일부 풀이

--- java-practice/src/ch07/sec03/exam02/SmartPhone.java
+++ java-practice/src/ch07/sec03/exam02/SmartPhone.java
@@ -1,4 +1,8 @@
 package ch07.sec03.exam02;
 
-public class SmartPhone {
+public class SmartPhone extends Phone {
+    public SmartPhone(String model, String color) {
+        super(model, color);
+        System.out.println("SmartPhone(String model, String color) 생성자 실행");
+    }
 }
--- java-practice/src/ch07/sec03/exam02/SmartPhoneExample.java
+++ java-practice/src/ch07/sec03/exam02/SmartPhoneExample.java
@@ -1,4 +1,10 @@
 package ch07.sec03.exam02;
 
 public class SmartPhoneExample {
+    public static void main(String[] args) {
+        SmartPhone sp = new SmartPhone("아이폰", "검정");
+
+        System.out.println("핸드폰 모델: " + sp.model);
+        System.out.println("핸드폰 색상: " + sp.color);
+    }
 }
